29e90e7d5f258ec6fe3cc060c1da2cd6c025daa4
Rafael Batista <rafaelfreba@gmail.com>
2022-12-19 16:29:21 -0400

    Criando a base de dados

---
+++ database/migrations/2022_12_19_200637_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('cpf')->unique();
+            $table->bigInteger('cns')->unique();
+            $table->string('name',255);
+            $table->date('birth');
+            $table->string('email',150);
+            $table->bigInteger('phone');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ database/migrations/2022_12_19_201059_create_counties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counties', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('ibge')->unique();
+            $table->string('name',255);
+            $table->char('fu',2)->default('MT');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counties');
+    }
+};
