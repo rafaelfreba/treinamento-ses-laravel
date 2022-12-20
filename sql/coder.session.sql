584e48db89d1467099b4d25639f2d5834b48a1c3
Rafael Batista <rafaelfreba@gmail.com>
2022-12-20 14:46:52 -0400

    Populando tabelas com dados para teste

---
+++ sql/coder.session.sql
@@ -0,0 +1,2 @@
+--cria o banco de dados
+CREATE SCHEMA `bd-cad-paciente` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ sql/insert_patients.sql
@@ -0,0 +1,122 @@
+use `bd-cad-paciente`;
+
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("60140588790","913026580737116","Elizabeth Justice","1995-07-03","mauris@protonmail.com","465356831",87),
+  ("55646260732","534662084796787","Norman Mayo","1998-10-22","at.velit.pellentesque@icloud.net","843440824",133),
+  ("81363711934","641915234771941","Nevada Reeves","1992-11-06","vel.lectus@icloud.net","868144887",15),
+  ("22417356165","865804108957481","Ruth Cantu","1999-02-24","dui.nec@google.org","095524134",91),
+  ("48784428247","535134648723647","Lucas Lara","1991-06-09","rhoncus@google.com","885372735",116),
+  ("66276831695","865858307555973","Owen Cabrera","1998-10-27","augue.sed@outlook.org","974634556",4),
+  ("18568233257","623515441268557","Germane Garner","1992-01-09","adipiscing@google.org","440742798",26),
+  ("61284829165","589529132385805","Meghan Burks","1991-09-14","dapibus.rutrum@outlook.ca","682376386",73),
+  ("52567328514","679763650791483","Aurelia Owen","1996-01-31","vitae@google.couk","156567453",28),
+  ("13411294505","701814971286296","Blake Guy","1994-06-13","dui@google.couk","387840436",15);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("62857163114","306818226459683","Audrey Compton","1993-03-14","nunc.lectus.pede@yahoo.org","983446486",19),
+  ("49724877037","407154758830434","Deirdre Cooley","1992-09-14","phasellus.elit@hotmail.edu","878582433",34),
+  ("92327464826","523843747668386","Bethany Sanchez","1996-09-10","vehicula.et.rutrum@google.org","487242413",23),
+  ("39357102240","321262831875457","Emi Sanchez","1998-05-26","eget.venenatis@yahoo.couk","442562269",57),
+  ("69144598242","637862890743036","Patricia Schwartz","1998-08-22","lorem.sit@hotmail.ca","622733803",110),
+  ("57563598377","013752788085337","Macaulay Conrad","1995-01-22","sodales@protonmail.org","146424547",121),
+  ("86505352596","521352399730754","Ursula Montoya","1999-07-10","auctor.odio@icloud.edu","306288168",103),
+  ("87287993797","693646032355464","Alana Rosa","1992-06-04","malesuada.vel.venenatis@yahoo.ca","174840113",30),
+  ("45321577525","673930247688814","Hall Brady","1999-05-21","suspendisse.dui@icloud.com","685003130",57),
+  ("40385321891","250478017153780","Victor Pace","1997-05-11","libero@protonmail.ca","763821244",88);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("42250381538","476538836417215","Dale Shepherd","1998-06-02","adipiscing@aol.com","535691383",3),
+  ("22478224012","262943617147486","Xenos Hutchinson","1998-06-16","semper.auctor.mauris@google.com","893703371",113),
+  ("24349376460","243088054947346","Austin Olsen","1996-05-08","donec.elementum@yahoo.com","484537883",99),
+  ("00931143191","118415538207284","Xander Duke","1998-02-07","dui@hotmail.com","574426594",121),
+  ("50572188540","974331953293738","Galvin Travis","1997-01-15","elit.etiam.laoreet@google.net","283536077",94),
+  ("02935287851","560494418211713","Neve Edwards","1993-03-10","enim.mauris@outlook.com","231185880",140),
+  ("27934854323","560512767795794","Rhoda Roman","1993-03-01","quisque@yahoo.net","038372162",111),
+  ("33334772617","641522284726649","Breanna Sanford","1999-09-13","non.sapien.molestie@hotmail.org","794585364",45),
+  ("57428335454","848448758748812","Magee Higgins","1992-06-04","libero@icloud.net","242646924",102),
+  ("51022994916","824348510568742","Courtney Lang","1994-02-06","sit.amet@google.com","780037093",49);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("14423789011","175433162950338","Joan Silva","1990-05-31","dictum.sapien.aenean@outlook.org","251965979",92),
+  ("69312186621","565287859111343","Hannah Moore","1996-01-13","eu@icloud.com","397454863",32),
+  ("52486975289","123329015914006","TaShya Williams","1994-05-19","maecenas.libero.est@yahoo.org","441997635",51),
+  ("82551307211","405437974848086","Cedric Drake","1996-05-09","tempus@protonmail.com","423419232",114),
+  ("37147222967","313855598862725","Peter Hughes","1994-11-23","phasellus@aol.net","117141625",31),
+  ("14054173883","666661821458683","Marshall Delgado","1995-10-14","ut.molestie@hotmail.ca","155158272",16),
+  ("66780726222","342484741153148","Barclay Emerson","1990-07-14","vitae@icloud.couk","120884391",102),
+  ("26572041635","621532775034855","Dustin Marquez","1990-08-04","nibh.sit@yahoo.ca","028569413",138),
+  ("53852811109","715640322522128","Jescie Robertson","1998-03-19","suspendisse.non@outlook.org","123708731",100),
+  ("31477517142","271268288247688","Adena Franco","1990-08-26","lacus.aliquam.rutrum@aol.edu","721112881",56);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("58343261544","137516785284533","Aphrodite Shaw","1995-10-14","vivamus@google.edu","112447223",13),
+  ("75841402688","651188593951028","Basil Reynolds","1993-09-15","fusce@aol.edu","723494625",134),
+  ("12768677843","768664768191742","Kevin Paul","1999-08-26","lorem.fringilla@outlook.net","138650586",111),
+  ("59800166773","118122416885813","Hasad Castillo","1992-10-28","sodales.at@aol.com","190399521",5),
+  ("84483732242","419785134512988","Duncan Harris","1998-05-28","maecenas.malesuada@aol.net","656873776",19),
+  ("58338611539","893326615826718","Wyatt Grimes","1990-08-09","nunc.sit@aol.net","163802256",13),
+  ("31137096177","608784816333940","Chancellor Mcpherson","1998-05-08","primis@yahoo.edu","266994480",138),
+  ("87336327822","838914291563770","Serina Gallegos","1993-01-30","nullam.vitae@protonmail.net","217232173",125),
+  ("61218291906","635708165973927","Alexandra Puckett","1995-07-15","vivamus.nisi@hotmail.ca","661413126",54),
+  ("96388145336","766982142089467","Raja Stephenson","1998-07-19","morbi.neque@yahoo.com","355051280",82);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("27841649366","650116176029725","Shafira Galloway","1994-12-04","cursus.non@yahoo.com","053263322",55),
+  ("20452713438","636625348197648","Lesley Cunningham","1998-04-21","ipsum.dolor@aol.org","287252877",101),
+  ("18881442155","924000146256421","Salvador West","1996-04-22","purus.maecenas.libero@hotmail.ca","904433110",44),
+  ("21681913117","786835392352946","Eleanor Donaldson","1991-06-01","aliquam.nisl.nulla@aol.edu","216271757",23),
+  ("12271511615","134774868242731","Carl Leonard","1997-05-26","feugiat.tellus.lorem@protonmail.com","546495015",124),
+  ("61726851386","624868627456112","Linus Witt","1998-01-16","mollis@outlook.com","056808714",137),
+  ("86436217358","870459531355183","Talon Todd","1990-07-22","cursus.diam@outlook.ca","730465433",114),
+  ("15161898482","455434295643366","Farrah Tucker","1998-02-26","vestibulum.accumsan.neque@yahoo.com","368362673",97),
+  ("74058936518","964197422766229","Prescott Newton","1997-05-08","nam.nulla.magna@protonmail.ca","079063565",54),
+  ("82217436921","142709971362336","Garrett Morales","1993-04-15","facilisis.facilisis@yahoo.edu","226427088",19);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("63267181306","662115168131738","Charissa Holder","1990-03-07","vel.faucibus@google.net","796791565",88),
+  ("54883916088","515224138458526","Lillian Byers","1992-02-12","cum@outlook.edu","063176060",35),
+  ("93303224798","558984260475271","Zorita Gordon","1990-07-23","nunc.quisque.ornare@icloud.couk","368963871",103),
+  ("62525168664","933735283124117","Leandra Ray","1997-09-07","eu@yahoo.net","854732022",33),
+  ("67653472312","467277687681831","Fuller Edwards","1996-07-01","eleifend@icloud.edu","115656474",99),
+  ("79624318815","525448944852337","Cassidy Mosley","1999-06-06","ac.ipsum@outlook.ca","465472868",114),
+  ("17951325765","864387280242252","Jorden Nicholson","1997-05-06","vel.est@icloud.ca","346954726",22),
+  ("93536151347","156633247231713","Luke Calderon","1998-05-14","integer.urna@hotmail.ca","321613811",76),
+  ("00876608525","148832877563686","Kerry Roach","1991-07-09","sed.et@google.edu","266507252",91),
+  ("45805200334","318630344773733","Autumn Carter","1996-01-02","cursus@protonmail.net","493440537",86);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("82881416541","319946486821680","Jared Moses","1996-09-03","tellus.id.nunc@outlook.org","877600389",122),
+  ("51384762846","280121668350035","Conan Hancock","1991-08-22","odio@icloud.ca","070445784",118),
+  ("97881173066","406356286625737","Burke Patton","1990-10-25","dictum.phasellus.in@yahoo.com","714174325",8),
+  ("72564439524","145277068373784","Rhiannon Stuart","1995-06-06","nisl@google.net","716853810",4),
+  ("97279627274","604537866767718","Lenore Kirkland","1990-01-02","quis@protonmail.org","251749288",110),
+  ("53563413275","224535358281983","Emerald Fernandez","1992-01-15","tristique.senectus@hotmail.com","355279436",19),
+  ("41044915494","290798108543953","Cedric Peck","1997-08-29","hendrerit@outlook.ca","554173334",95),
+  ("30710927366","671475421612678","Maia Schwartz","1991-02-05","eget.massa@hotmail.edu","341751521",113),
+  ("41492900258","420508544175460","Garrett Allison","1998-09-14","et.malesuada.fames@outlook.org","368872770",25),
+  ("33446320451","519449156113184","Seth Dudley","1995-12-16","orci.lobortis@hotmail.edu","128643952",114);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("87625538842","814187107866176","Gannon Schwartz","1994-07-27","mauris.eu@hotmail.org","576519643",63),
+  ("86623041539","823636396076545","Lara Reese","1993-08-09","pellentesque.eget.dictum@outlook.couk","151913372",22),
+  ("31315681035","293541752249486","Eve Hatfield","1990-12-15","mauris@outlook.org","461368062",63),
+  ("65832816861","137303848708618","Beck Richmond","1994-08-07","cursus.a@hotmail.com","821294374",8),
+  ("66193048643","732390384885143","Ria Richardson","1993-05-09","penatibus.et@icloud.org","329555188",21),
+  ("04028132584","629214068467733","Arden Richardson","1992-03-04","lobortis.class@protonmail.org","673484432",3),
+  ("64111351682","328852513980673","Yuri Fulton","1991-11-15","mauris.vestibulum@icloud.edu","591303313",13),
+  ("73835533607","233871352799069","Mason Parks","1999-03-27","sociis.natoque.penatibus@icloud.edu","337737645",125),
+  ("03637645971","046319587293491","Darryl Tyson","1995-05-23","eget.ipsum.donec@protonmail.edu","733964874",101),
+  ("38924832392","349151821356048","Geoffrey Rowland","1993-05-27","curabitur.consequat@outlook.org","728675161",97);
+INSERT INTO `patients` (`cpf`,`cns`,`name`,`birth`,`email`,`phone`,`county_id`)
+VALUES
+  ("25483785740","961754712767871","Forrest Osborn","1997-03-28","pharetra.sed@hotmail.couk","024114047",109),
+  ("16432831333","855301934387886","Heather Acosta","1996-06-21","velit@hotmail.net","444440915",67),
+  ("88796416375","627835671134059","Phoebe Zimmerman","1996-10-17","mauris.elit.dictum@protonmail.net","721738424",79),
+  ("96868504436","736762453664806","Julie David","1990-02-28","vitae.risus@aol.couk","688456453",60),
+  ("45451648687","174575617882260","Warren Cooke","1994-03-16","donec.est@outlook.couk","652472313",53),
+  ("41735535228","451683331413735","Sacha Rhodes","1991-06-10","duis.mi@hotmail.couk","211352757",100),
+  ("06129165044","741410277421355","Camden Mcclure","1991-12-18","dolor.elit@hotmail.edu","485924194",100),
+  ("67533252053","456368737529241","Hayes Allison","1992-09-15","nec.quam.curabitur@outlook.org","797335272",69),
+  ("57232281255","175323696935834","William Patrick","1996-06-26","egestas.blandit@outlook.com","447812686",7),
+  ("03436254636","324191157374699","Zahir Frank","1990-09-04","sed.et@yahoo.net","444235940",72);
