1e93752b779eea82f43f24115a62e637c25035ea
Rafael Batista <rafaelfreba@gmail.com>
2022-12-21 16:35:57 -0400

    Refatorando as rotas com route resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CountyController;
 use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/patients', [PatientController::class, 'index'])->name('patients.index');
+// Route::get('/patients', [PatientController::class, 'index'])->name('patients.index');
+// Route::get('/patients', [PatientController::class, 'create'])->name('patients.create');
+// Route::get('/patients', [PatientController::class, 'edit'])->name('patients.edit');
+// Route::post('/patients', [PatientController::class, 'store'])->name('patients.store');
+// Route::put('/patients', [PatientController::class, 'update'])->name('patients.update');
+// Route::delete('/patients', [PatientController::class, 'destroy'])->name('patients.destroy');
+
+Route::resource('patients', PatientController::class);
+Route::resource('counties', CountyController::class);
+
 
 
